412efa339830ed6f4489128e9fc0b240dd7f7106
Sthealth <AW@example.com>
2025-10-07 10:10:02 -0400

    Revert "add main code"
    This reverts commit c1cff1b81a9ac063dfc047889c241216f97568cc.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,28 +1,4 @@
 package com.pluralsight;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter the file you want to read: ");
-        String story = myScanner.nextLine();
-
-        try {
-            BufferedReader myReader = new BufferedReader(new FileReader(story));
-            String line;
-            int lineNumber = 1;
-
-            while ((line = myReader.readLine()) != null) {
-
-                System.out.println(lineNumber + ".   " + line);
-                lineNumber++;
-            }
-            myReader.close();
-        } catch (Exception exception) {
-            System.out.println("File not found!");
-            System.out.println(exception.getMessage());
-        }
-    }
+public class Maiin {
 }
